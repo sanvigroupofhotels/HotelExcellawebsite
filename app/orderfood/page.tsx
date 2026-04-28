0c1a611d7a026b3abbe3ea5e590fb888a8895bcf
sanvigroupofhotels <sanvigroupofhotels@gmail.com>
2026-04-28 10:32:55 +0530

    Point /orderfood to correct food menu deployment

--- app/orderfood/page.tsx
+++ app/orderfood/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function OrderFoodPage() {
-  redirect(
-    "https://wa.me/919985908131?text=Hi%2C%20I%20am%20staying%20at%20Hotel%20Excella%20and%20I%20would%20like%20to%20place%20a%20food%20order.",
-  )
+  redirect("https://hotelexcellafoodmenu.vercel.app")
 }
